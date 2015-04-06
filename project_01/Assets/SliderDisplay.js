97b32cf953a9c2300a970ea8f39bcd324cc614e0
JoWylie2 <Joanne_Wylie@emerson.edu>
2015-04-06 11:34:22 -0400

    GUI prefabs

---
+++ project_01/Assets/SliderDisplay.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+var textbox : Text;
+var slider : Slider;
+
+function sliderdisplay (x : float) {
+	textbox.text = x.ToString();
+
+}
---
+++ project_01/Assets/Text_handler.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+var greeting : Text;
+
+var namefield : InputField;
+
+function NameInsert () {
+greeting.text = "Hello " + namefield.text;
+
+}
