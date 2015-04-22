80eb5159fe421c016d2839546a4055491c31ecbf
JoWylie2 <Joanne_Wylie@emerson.edu>
2015-04-22 18:47:41 -0400

    add cursor, fix destroy script

--- project_01/Assets/CrossHairCursor.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
- 
-var cursorTexture : Texture2D;
-var hotSpot : Vector2 = Vector2.zero;
- 
-function Start () {
-   Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.ForceSoftware);
-
-}
